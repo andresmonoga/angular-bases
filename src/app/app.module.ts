75e68dcd9cfa0804561ae51b279b15eb4585f716
Andres Monoga <andresmonoga07@gmail.com>
2024-04-04 10:37:43 -0500

    fin de la sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/components/counter/counter.module';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/hero/list/list.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
